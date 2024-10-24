796d1c7a6907c557eccf9f99c9d7ad9adf113f9c
Андрей <a.skobliakov@gmail.com>
2024-10-25 01:48:29 +0300

    feat: created layouts, added login and registration forms, created logo component

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
---
+++ components/BaseLogo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="['logo', customClass]">
+      <slot>Chattio</slot>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    customClass: {
+      type: String,
+      default: 'black'
+    }
+  });
+  </script>
+  
+  <style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Birthstone:wght@400&display=swap');
+  
+  .logo {
+    font-family: 'Birthstone', cursive;
+    font-size: 5rem;
+    text-align: center;
+    font-weight: 400;
+  }
+  
+  </style>
+  
